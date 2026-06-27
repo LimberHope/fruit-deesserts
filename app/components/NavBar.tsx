f996beb5f770a2afe47a15a138d3de651fc87689
esperanza-desing <esperanzamamani266@gmail.com>
2026-06-27 10:54:16 -0400

    change height navigator

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -3,22 +3,22 @@ import Image from "next/image";
 
 const NavBar = () => {
   return (
-    <nav className="flex items-center justify-center p-4 bg-[#F2D5D5] h-[50px]">
+    <nav className="flex items-center justify-center p-4 bg-[#F2D5D5] h-[125px]">
       <Link href="/" className="text-xl flex items-center">
         <Image className="hover:scale-110 transition-all duration-300"
           src={"/img/fresa.svg"}
           alt="fresa"
-          width={30}
-          height={30}
+          width={75}
+          height={75}
         />
-        <p className="hover:border-b-2 hover:border-[#4AE7F0]">
+        <p className="hover:border-b-2 hover:border-[#4AE7F0] text-[50px]">
           fruit dessert
         </p>
         <Image className="hover:scale-110 transition-all duration-300"
           src={"/img/arandano.svg"}
           alt="arandano"
-          width={30}
-          height={30}
+          width={75}
+          height={75}
         />
       </Link>
     </nav>
