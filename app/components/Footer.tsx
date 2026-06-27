490b58d211110b5d046cc31df1169834abeeecd7
esperanza-desing <esperanzamamani266@gmail.com>
2026-06-27 15:43:29 -0400

    update footer component

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,16 +1,14 @@
 import Image from "next/image";
 const Footer = () => {
   return (
-    <footer className="mt-auto w-full bg-[#F2D5D5] text-gray-800">
-      {/* <div className="max-w-6xl mx-auto px-4 py-8"> */}
-
+    <footer className="mt-auto w-full bg-[#F2D5D5] text-gray-800 py-12">
       <div className="text-center flex items-center justify-center">
         <div className="basis-1/3">
-          <h3 className="font-bold text-lg mg-3">fruit desserts</h3>
-          <p className="text-sm">los mejores postres de frutas en la paz</p>
+          <h3 className="font-bold text-2xl md:text-2xl mb-4 uppercase tracking-wile">fruit desserts</h3>
+          <p className="text-sm">Los mejores postres de frutas en la ciudad de LA PAZ,comprame case bien riquito te voy a dar</p>
         </div>
         <div className="basis-1/3">
-          <h3 className="font-bold text-lg mg-3">contactanos:</h3>
+          <h3 className="font-bold text-2xl md:text-2xl mb-4 uppercase tracking-wide">contactanos:</h3>
           <ul className="space-y-2 text-sm">
             <li>
               <a href="Facebook:@fruitdesserts" className="hover:underline flex justify-center">Facebook:@fruitdesserts
@@ -22,7 +20,6 @@ const Footer = () => {
                 />
               </a>
             </li>
-
             <li>
               <a href="whattssap:64065835" className="hover:underline flex justify-center">whatssap:64065835
                 <Image className="hover:scale-110 transition-all duration-300"
