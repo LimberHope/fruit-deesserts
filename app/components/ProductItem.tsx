725d7f92b831735bb217889272f6c90d981f4171
LimberHope <limbermv2@gmail.com>
2026-06-27 21:27:50 -0400

    add content seccion for product item

--- app/components/ProductItem.tsx
+++ app/components/ProductItem.tsx
@@ -11,11 +11,22 @@ const ProductItem = ({
   return (
     <div className="fixed z-50 w-full h-full bg-[#080707]/50 flex items-center justify-center">
       <div className="absolute">
-        <img
-          src={image}
-          alt={title}
-          className={`rounded-t-lg scale-110 transition-all duration-300 w-full sm:w-[400px] md:w-[400px] lg:w-[500px] t-4`}
-        />
+        <div className="flex gap-0">
+          <div className="flex-1">
+            <img
+              src={image}
+              alt={title}
+              className={`rounded-t-lg w-full sm:w-[400px] md:w-[400px] lg:w-[500px] t-4`}
+            />
+          </div>
+          <div className="flex-1 bg-[#F2E6ED]/70 rounded-t-lg w-full p-4">
+            <h2>{title}</h2>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam,
+              quos.
+            </p>
+          </div>
+        </div>
         <Image
           src={"/img/search-minus.svg"}
           alt={title}
