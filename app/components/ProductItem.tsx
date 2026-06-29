7bcfc216483ba7b0d1f77b79fbbdfe1aebdb9fdb
LimberHope <limbermv2@gmail.com>
2026-06-29 15:24:46 -0400

    update product item component

--- app/components/ProductItem.tsx
+++ app/components/ProductItem.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+
 const ProductItem = ({
   image,
   title,
@@ -10,21 +11,36 @@ const ProductItem = ({
 }) => {
   return (
     <div className="fixed z-50 w-full h-full bg-[#080707]/50 flex items-center justify-center">
-      <div className="absolute">
-        <div className="flex gap-0">
-          <div className="flex-1">
-            <img
-              src={image}
-              alt={title}
-              className={`rounded-t-lg w-full sm:w-[400px] md:w-[400px] lg:w-[500px] t-4`}
-            />
-          </div>
-          <div className="flex-1 bg-[#F2E6ED]/70 rounded-t-lg w-full p-4">
+      {/* <div className="absolute w-full"> */}
+      <div>
+        {/* <article className="flex justify-center items-center">
+          <img
+            src={image}
+            alt={title}
+            className={`float-start rounded-t-lg w-full sm:w-[400px] md:w-[400px] lg:w-[500px]`}
+          />
+          <div className="bg-[#F2E6ED]/70 rounded-t-lg p-4 sm:w-[400px] md:w-[400px] lg:w-[500px]">
             <h2>{title}</h2>
             <p>
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam,
               quos.
             </p>
+          </div>
+        </article> */}
+        <div className="flex flex-col gap-2 p-8 sm:flex-row sm:items-center sm:gap-6 sm:py-4 bg-[#F2E6ED]/70">
+          <img
+            className="mx-auto block rounded-t-lg sm:mx-0 sm:shrink-0"
+            src={image}
+            alt=""
+          />
+          <div className="space-y-2 text-center sm:text-left">
+            <div className="space-y-0.5">
+              <p className="text-lg font-semibold text-black">Erin Lindford</p>
+              <p className="text-gray-500">Product Engineer</p>
+            </div>
+            <button className="border-purple-200 text-purple-600">
+              Message
+            </button>
           </div>
         </div>
         <Image
@@ -33,7 +49,7 @@ const ProductItem = ({
           width={50}
           height={50}
           onClick={handleClose}
-          className={`absolute top-0 right-0 p-1 hover:scale-110 transition-all duration-300 cursor-pointer`}
+          className={`absolute top-0 right-0 hover:scale-110 transition-all duration-300 cursor-pointer`}
         />
       </div>
     </div>
