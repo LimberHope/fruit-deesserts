82efed552dc9fb84b884bd948909344412530231
LimberHope <limbermv2@gmail.com>
2026-06-29 15:31:58 -0400

    add max width for product item image

--- app/components/ProductItem.tsx
+++ app/components/ProductItem.tsx
@@ -11,25 +11,10 @@ const ProductItem = ({
 }) => {
   return (
     <div className="fixed z-50 w-full h-full bg-[#080707]/50 flex items-center justify-center">
-      {/* <div className="absolute w-full"> */}
       <div>
-        {/* <article className="flex justify-center items-center">
-          <img
-            src={image}
-            alt={title}
-            className={`float-start rounded-t-lg w-full sm:w-[400px] md:w-[400px] lg:w-[500px]`}
-          />
-          <div className="bg-[#F2E6ED]/70 rounded-t-lg p-4 sm:w-[400px] md:w-[400px] lg:w-[500px]">
-            <h2>{title}</h2>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam,
-              quos.
-            </p>
-          </div>
-        </article> */}
         <div className="flex flex-col gap-2 p-8 sm:flex-row sm:items-center sm:gap-6 sm:py-4 bg-[#F2E6ED]/70">
           <img
-            className="mx-auto block rounded-t-lg sm:mx-0 sm:shrink-0"
+            className="mx-auto block rounded-t-lg sm:mx-0 sm:shrink-0 max-w-[600px]"
             src={image}
             alt=""
           />
