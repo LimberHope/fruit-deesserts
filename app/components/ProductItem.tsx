492b3b28290374101ecd1002638ddf008f5d7a19
LimberHope <limbermv2@gmail.com>
2026-07-06 13:49:10 -0400

    move search minus icon

--- app/components/ProductItem.tsx
+++ app/components/ProductItem.tsx
@@ -13,7 +13,7 @@ const ProductItem = ({
 }) => {
   return (
     <div className="fixed z-50 w-full h-full bg-[#080707]/50 flex items-center justify-center">
-      <div>
+      <div className="relative">
         <div className="flex flex-col gap-2 p-8 sm:flex-row sm:items-center sm:gap-6 sm:py-4 bg-[#F2E6ED]/70">
           <img
             className="mx-auto block rounded-t-lg max-w-[400px] md:max-w-[500px] lg:max-w-[600px]"
